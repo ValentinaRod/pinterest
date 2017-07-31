3cec85021e7b7e9ce0a309ca822ccfb721ecc6ec
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-07-31 03:00:09 -0400

    creando css y js img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(){
-	
+	for(var i =0; i <20 ; i++){
+		var img=$('<img>').attr('src', 'dis/img' + data[i].image_url);
+		var cards=$('<div class="cards" data-toggle="modal" data-target="#myModal">');
+		var titulo=$('<h4>').attr('class', "title");
+		titulo.text(data[i].title);
+		var descripcion=$('<p>').atrr('class','descripciones');
+		descripcion.text(data[i].description);
+		var divUsuario=$('<div>').atrr('class', 'user');
+		var user=$('<p class="user">').append('<p><i class="fa fa-user-circle" aria-hidden="true"></i>'+data[i].username + '</p>')
+
+
+
+
+
+
+
+
+
+
+	}	
 }
